b0ca9bc1b8a457c3d28e441f1b9b7ff49b5df6e5
Fuad Hasan <fuadgit@gmail.com>
2024-08-12 20:45:53 +0600

    Add code
    malti line string

--- String/Normal/str_basic.py
+++ String/Normal/str_basic.py
@@ -13,3 +13,12 @@ print(s)
 s1 = "Fuad"
 s2 = "Hasan"
 print(s1 +" "+ s2)
+
+s = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.
+"""
+
+print(s)
+
