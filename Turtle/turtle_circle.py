8f36a3d6353ad5686ef1bd0f9d87c13f4d3b8747
Fuad Hasan <fuadgit@gmail.com>
2024-08-17 20:58:46 +0600

    Delete Turtle directory

--- Turtle/turtle_circle.py
+++
@@ -1,7 +0,0 @@
-import turtle
-
-my_turtle = turtle.Turtle()
-
-my_turtle.circle(50)
-
-turtle.done()
--- Turtle/turtle_star.py
+++
@@ -1,9 +0,0 @@
-import turtle
-
-my_turtle = turtle.Turtle()
-
-for _ in range(5):
-    my_turtle.forward(100)
-    my_turtle.right(144)
-
-turtle.done()
