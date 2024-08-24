ce92403765aaa02db3635c3e75a64cce0905a968
Fuad Hasan <fuadgit@gmail.com>
2024-08-24 08:12:40 +0600

    Add files via upload

---
+++ OOP/Class-Object/first_time.py
@@ -0,0 +1,26 @@
+class car_1:
+       name = "Premio"
+       color = "White"
+       
+       def start():
+              print("Starting the engine")
+              
+print("Name of car: " + car_1.name)
+print("Color of car: " + car_1.color)
+
+car_1.start()              
+
+class car_2:
+       name = ""
+       color = ""
+       
+       def start():
+              print("Starting the engine")
+              
+car_2.name = "Axio"
+car_2.color = "Black"
+
+print("Name of car: " + car_2.name)
+print("Color of car: " + car_2.color)
+
+car_2.start()              
